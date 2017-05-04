bd25448c1507e9dc12aa24bd5e5c51a99341315b
cyberworm3285 <cyberworm3285@web.de>
2017-05-05 00:41:03 +0200

    boolean,yo

--- NeuroNet2/Program.cs
+++ NeuroNet2/Program.cs
@@ -15,15 +15,20 @@ namespace NeuroNet2
     {
         static void Main(string[] args)
         {
+            B();
+        }
+
+        static void A()
+        {
             FullMesh<double> mesh = new FullMesh<double>(
-                inputCounts:            2,
-                neuroCounts:            new int[] { 5, 5, 5, 1 },
-                errorFunction:          (a, b) => Math.Abs(a-b),
-                activationFunction:     d => d, 
-                adderFunction:          (a,b) => a + b, 
-                weightingFunction:      (a,b) => a * b,
-                defaultWeight:          0.5
-                );
+            inputCounts: 2,
+            neuroCounts: new int[] { 5, 5, 5, 1 },
+            errorFunction: (a, b) => Math.Abs(a - b),
+            activationFunction: d => d,
+            adderFunction: (a, b) => a + b,
+            weightingFunction: (a, b) => a * b,
+            defaultWeight: 0.5
+            );
 
             double[][] input = new double[][]
             {
@@ -44,7 +49,7 @@ namespace NeuroNet2
             for (int i = 0; i < 20; i++)
                 mesh.LearnIteration(new DeepRandom<double>(250, d => d + GlobalRandom.Get.NextDouble() * 0.4 - 0.2) { PrintProgress = true }, input, output, -0.0001);
             for (int i = 0; i < 10000 && mesh.LastError > 0.00001; i++)
-                Console.WriteLine(mesh.LearnIteration(new Evolution<double>(d => d + GlobalRandom.Get.NextDouble() * 0.4 - 0.2),input, output, -0.00001));
+                Console.WriteLine(mesh.LearnIteration(new Evolution<double>(d => d + GlobalRandom.Get.NextDouble() * 0.4 - 0.2), input, output, -0.00001));
             //mesh.LearnIteration(new BruteForce<double>(d => d + GlobalRandom.Get.NextDouble() * 0.4 - 0.2) { PrintProgress = true }, input, output, 0.01);
 
             Array.ForEach(input, d => Console.WriteLine(Format(d) + " : " + Format(mesh.Calc(d))));
@@ -52,6 +57,56 @@ namespace NeuroNet2
             Console.ReadKey();
         }
 
-        static string Format(double[] d) => "[" + string.Join(",", d.Select(dd => dd.ToString())) + "]";
+        static void B()
+        {
+            FullMesh<bool> mesh = new FullMesh<bool>(
+                inputCounts : 2,
+                neuroCounts : new int[] { 10, 10, 1 },
+                errorFunction : (a,b) => (a != b) ? 1 : 0,
+                activationFunction : b => b,
+                adderFunction : (a, b) => a || b,
+                weightingFunction : (a,b) => a || b,
+                defaultWeight: false
+                );
+
+            Func<bool, bool, bool>[] funcs = new Func<bool, bool, bool>[]
+            {
+                (a,b) => !(a&&b),
+                (a,b) => a&&b,
+                (a,b) => !(a||b),
+                (a,b) => a||b
+            };
+
+            mesh.Neurons.ForEach(n => n.ForEach(nn => nn.WeightingFunction = funcs[GlobalRandom.Get.Next(0, funcs.Length)]));
+
+            bool[][] input = new bool[][]
+            {
+                new[] { false, false },
+                new[] { false, true },
+                new[] { true, false },
+                new[] { true, true },
+            };
+
+            bool[][] output = new bool[][]
+            {
+                new[] { false },
+                new[] { true },
+                new[] { true },
+                new[] { false },
+            };
+
+
+            mesh.LearnIteration(new BruteForce<bool>(b => !b) { PrintProgress = true }, input, output, 0.01);
+            //for (int i = 0; i < 20; i++)
+            //    mesh.LearnIteration(new DeepRandom<bool>(250, d => !d) { PrintProgress = true }, input, output, -0.0001);
+            //for (int i = 0; i < 10000 && mesh.LastError > 0.00001; i++)
+            //    Console.WriteLine(mesh.LearnIteration(new Evolution<bool>(b => !b), input, output, -0.00001));
+
+            Array.ForEach(input, d => Console.WriteLine(Format(d) + " : " + Format(mesh.Calc(d))));
+
+            Console.ReadKey();
+        }
+
+        static string Format<T>(T[] d) => "[" + string.Join(",", d.Select(dd => dd.ToString())) + "]";
     }
 }
